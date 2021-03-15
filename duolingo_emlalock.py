48c2c964cb13538d09f366622eb060ea92e57765
s4b3r1234 <80568934+s4b3r1234@users.noreply.github.com>
2021-03-14 23:09:16 -0400

    Update duolingo_emlalock.py

--- duolingo_emlalock.py
+++ duolingo_emlalock.py
@@ -2,6 +2,7 @@
 # coding=utf-8
 # Written by s4b3r1234 for the emlalock discord - 2021
 # I know it's not pretty... but it works.
+# March 14, 2021 - Fixed an issue where the streak value would hold as what it was when the script was first run. This was purely due to me not knowing anything.
 
 import duolingo
 import time
@@ -10,31 +11,37 @@ import requests
 import sys
 import configparser
 
-if len(sys.argv) < 2:
-    sys.stdout.write("\nUsage: nohup python3 duo_amlalock.py config.txt & \n")
-    sys.stdout.write("Make sure that all required values are filled out in config.txt. All values are required except for emlalock-holder-api, which is only used if time is being subtracted. \n")
-    sys.stdout.write("Time can be added or subtracted in the format W1 for 1 week or D1 for 1 day or H1 for 1 hour or M1 for 1 minute.")
-    sys.exit(1)
 # Setup the config parser
 config = configparser.RawConfigParser(allow_no_value=True)
 config.read('config.txt')
 
-# Setup DuoLingo
+# Check for configs. I'm not actually sure this works, I'm not sure why it's returning if the value exists, not if there is a value.
+# If anyone knows why this is or how I can fix it please let me know.
 if config.has_option('DUOLINGO', 'duo_user') == True:
-    duo_user = config['DUOLINGO']['duo_user']
+    pass
 else:
     print("Missing value for duo_user. Please add it to config.txt")
     sys.exit(1)
 
 if config.has_option('DUOLINGO', 'duo_pass') == True:
-    duo_pass = config['DUOLINGO']['duo_pass']
+    pass
 else:
     print("Missing value for duo_pass. Please add it to config.txt")
     sys.exit(1)
 
-lingo = duolingo.Duolingo(duo_user, duo_pass)
-streak_info = lingo.get_streak_info()
-streak_value = streak_info.get('streak_extended_today')
+# Moved the streak checker into a callable function. Previously it was holding the value (Duh, stupid me!)
+def streak_check():
+    duo_user = config['DUOLINGO']['duo_user']
+    duo_pass = config['DUOLINGO']['duo_pass']
+    lingo = duolingo.Duolingo(duo_user, duo_pass)
+    streak_info = lingo.get_streak_info()
+    streak_value = streak_info.get('streak_extended_today')
+    if streak_value != True and streak_value != False:
+        print("Invalid Return")
+        return streak_value
+    else:
+        return streak_value
+        print(streak_value)
 
 # Setup Emlalock
 if config.has_option('EMLALOCK', 'emlalock_user_id') == True:
@@ -54,8 +61,13 @@ else:
     sys.exit(1)
 
 # These options are not required in config.txt, but are required if time will be subtracted on successful streak increase.
-emlalock_holder_api = config['EMLALOCK']['emlalock_holder_api']
 duration_sub = config['EMLALOCK']['duration_sub']
+
+if config.has_option('EMLALOCK', 'emlalock_holder_api') == True:
+    emlalock_holder_api = config['EMLALOCK']['emlalock_holder_api']
+    holder_api_exists = True
+else:
+    holder_api_exists = False
 
 def add_time():
     payload = {'userid': emlalock_user_id, 'apikey': emlalock_user_api, 'value': duration_add}
@@ -63,7 +75,7 @@ def add_time():
     response.text
 
 def sub_time():
-    if config.has_option('EMLALOCK', 'emlalock_holder_api') == True:
+    if holder_api_exists == True:
         payload = {'userid': emlalock_user_id, 'apikey': emlalock_user_api, 'holderapikey': emlalock_holder_api, 'value': duration_sub}
         response = requests.get("https://api.emlalock.com/sub", params=payload)
         response.text
@@ -71,17 +83,18 @@ def sub_time():
         pass
 
 def did_user_learn():
-    if streak_value == True:
-        if config.has_option('EMLALOCK', 'emlalock_holder_api') == False:
-            sys.stdout.write("No Time is Added Today, Good Work!")
-        else:
+    if streak_check() == True:
+        if config.has_option('EMLALOCK', 'emlalock_holder_api') == True:
             sub_time()
-            sys.stdout.write(duration_sub, "has been removed")
+            print(duration_sub, "has been removed")
+        else:
+            if config.has_option('EMLALOCK', 'emlalock_holder_api') == False:
+                sys.stdout.write("No Time is Added Today, Good Work!")
     else:
-        sys.stdout.write(duration_add, "has been added")
+        print(duration_add, "has been added")
         add_time()
 
-#Setup scheduling
+# Setup scheduling
 time_to_run = config['RUNTIME']['time_to_run']
 schedule.every().day.at(time_to_run).do(did_user_learn)
 
